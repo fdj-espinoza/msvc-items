13778ebf21456f85c1409610016da8fcdfe73eb5
Francisco Espinoza <fdj.espinoza@gmail.com>
2025-09-25 00:22:11 -0700

    fin feing client

---
+++ src/main/java/com/example/springcloud/msvc/items/services/ItemServiceFeign.java
@@ -0,0 +1,40 @@
+package com.example.springcloud.msvc.items.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.springcloud.msvc.items.clients.ProductFeingClient;
+import com.example.springcloud.msvc.items.models.Item;
+import com.example.springcloud.msvc.items.models.Product;
+
+import feign.FeignException;
+
+@Service
+public class ItemServiceFeign implements ItemService {
+
+    @Autowired
+    private ProductFeingClient client;
+
+    @Override
+    public List<Item> findAll() {
+        return client.findAll()
+                .stream()
+                .map(product -> new Item(product, new Random().nextInt(10) + 1))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        try {
+            Product product = client.details(id);
+            return Optional.of(new Item(product, new Random().nextInt(10) + 1));
+        } catch (FeignException e) {
+            return Optional.empty();
+        }
+    }
+}
